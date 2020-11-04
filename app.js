d79973bd4a3355c19f7a681ad75b2ba4e131d8d6
IdoGo15 <idogolan15@gmail.com>
2020-11-04 18:43:25 +0200

    Added hover over the team rows & new teamPage html

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+
 const table = document.getElementById('tTable');
+let flag = false;
 
-
-function getTeams() {
-  fetch('https://api.sportsdata.io/v3/nba/scores/json/teams?key=4a3c1ad9e42b4a3b892aa67ac688860b')
+// Get Teams 
+async function getTeams() {
+  await fetch('https://api.sportsdata.io/v3/nba/scores/json/teams?key=4a3c1ad9e42b4a3b892aa67ac688860b')
     .then((res) => res.json())
     .then((data) => {
-      console.log(data[0].Name);
       let output = `
         <thead>
           <tr style="padding: 15px 50px;">
@@ -19,7 +20,7 @@ function getTeams() {
       `;
       data.forEach(function(team) {
         output += `
-        <tr style="background:#${team.SecondaryColor}; color:#${team.PrimaryColor}; padding-left:50px; padding-right:50px;">
+        <tr id="teamRow" style="background:#${team.SecondaryColor}; color:#${team.PrimaryColor}; padding-left:50px; padding-right:50px;">
           <th scope="row"><img src="${team.WikipediaLogoUrl}"></th>
           <td>${team.Name}</td>
           <td>${team.City}</td>
@@ -27,12 +28,16 @@ function getTeams() {
         </tr>
         `;
       })
-
+      flag = true;
       output+= `</tbody>`
-
       tTable.innerHTML = output;
 })
 }
 
 getTeams();
+
+
+
+
+
 
